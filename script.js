c04a7c1d876e4b1de050da553fd1f0c246c9c629
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-11-09 10:33:19 -0800

    added to code

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 // Assignment code here
-var chars = "1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var chars = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChars ="!@#$%^&*()";
+var numbers = "1234567890";
 var min=12;
 var max= 128;
 var password = "";
+
+var password = prompt("To generate a password, would you like to add characters:", "yes or no");
+
+var password = confirm("Would you like to add another character?");
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var chars = ["1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-  var min=12;
-  var max= 128;
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
