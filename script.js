00a3021803d3023a4202c65c255855f625d2e26a
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-11-10 00:15:51 -0800

    added vars  to the function

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-// data needed to generate password 
-
-var lowerCase =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*' , '(', ')', '"' ]
-var numbers =  ['1', '2', '3', '4', '5' , '6', '7' , '8', '9', '0' ]
+// Assignment Code Here
 
 
 
@@ -17,11 +12,14 @@ var specialChars = ("Would you like to add special characters?");
 
 
 function generatePassword(){
-
-  var charsAmount = (prompt("To generate a password, how many characters would you like to add?", "NOTE: must between 8 - 128 characters"));
-  while
-  (charsAmount <= 8 || confirmLength >= 128 ) {
-    alert("Password length must be between 8-50 characters Try again");
+  var lowerCase =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var upperCase =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*' , '(', ')', '"' ];
+  var numbers =  ['1', '2', '3', '4', '5' , '6', '7' , '8', '9', '0' ];
+  
+  charsAmount = (prompt("To generate a password, how many characters would you like to add?", "NOTE: must between 8 - 128 characters"));
+  if (charsAmount <= 8 || confirmLength >= 128 ) {
+    alert("Password length must be between 8-128 characters Try again");
     var charsAmount = (prompt("How many characters would you like your password to contain?"));
     } 
 
