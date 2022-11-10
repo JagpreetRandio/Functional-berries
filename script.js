d1a1544131f4b489c7cc3c9e63eb3b1198999f89
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-11-10 15:58:01 -0800

    added comments to code

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 // Assignment code here
 function generatePassword() {
- 
+
+//  Options for the random password 
+
   var numberCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
   var possibleCharacters = [];
 
-  // get input and validate
+  // get input and validate the number of characters for password 
+
   charactersForRandomPassword = prompt("How many characters would you like for your password? NOTE: Must be between 8-128 characters");
   if (charactersForRandomPassword < 8 || charactersForRandomPassword > 128) {
-    return "Please choose a valid number of characters.";
+    return "Please choose a valid number of characters. Try Again!";
   } else if (isNaN(charactersForRandomPassword)) {
+    // if user doesn't chose a number between 8-128, a prompt will show up to let user enter a vaild number 
     charactersForRandomPassword = prompt("Please enter a valid number to generate a password.");
   }
   else {
+    // lets user know how many characters will be used for thier random password 
     alert("Your password will be " + charactersForRandomPassword + " characters long!");
     
   }
 
+// confirming lowercase characters 
   lowerCase = confirm("Would you like to add lowercase characters?");
   if (lowerCase) {
     alert("Your password will have lowercase characters!");
@@ -27,6 +33,7 @@ function generatePassword() {
     alert("Your password will NOT have lowercase characters!");
   }
 
+// confirming uppercase characters 
   upperCase = confirm("Would you like to add uppercase characters?");
   if (upperCase) {
     alert("Your password will have uppercase characters!");
@@ -35,6 +42,7 @@ function generatePassword() {
     alert("Your password will NOT have uppercase characters!");
   }
 
+  // confirming numbers 
   numbers = confirm("Would you like to add numbers?");
   if (numbers) {
     alert("Your password will have numbers!");
@@ -42,7 +50,7 @@ function generatePassword() {
   else {
     alert("Your password will NOT have numbers!");
   }
-
+// confirming special characters
   specialChars= confirm("Would you like to add special characters?");
   if (specialChars) {
     alert("Your password will have special characters!");
@@ -51,11 +59,12 @@ function generatePassword() {
     alert("Your password will NOT have special characters!");
   }
 
+  // if no options is chosen, a message will tell user to select one character type 
   if (lowerCase === false && upperCase === false && numbers === false && specialChars === false) {
-    return "Please select at least one character type.";
+    return "Please select at least one character type. Try Again!";
   };
 
-  // group selected characters
+  // group selected characters and concat is used to group all options together 
   if (lowerCase) {
     possibleCharacters = possibleCharacters.concat(lowercaseCharacters);
   }
@@ -69,11 +78,10 @@ function generatePassword() {
     possibleCharacters = possibleCharacters.concat(specialCharacters);
   }
 
-  // pick random cards out of new pool for length of password
+  // picks random characters out for the random password 
   let randomPassword = ""
   for (let i = 0; i <  charactersForRandomPassword; i++) {
     let rng =[Math.floor(Math.random() * possibleCharacters.length)];
-    // or finalPassword += possibleCharacters[rng];
     randomPassword = randomPassword + possibleCharacters[rng];
   }
   return randomPassword;
