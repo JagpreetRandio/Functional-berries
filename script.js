0cf6baa2c6a8f80f47c05273bccaae79317ebc75
JagpreetRandio <115323708+JagpreetRandio@users.noreply.github.com>
2022-11-10 00:42:12 -0800

    added the confirms to code

--- script.js
+++ script.js
@@ -1,30 +1,57 @@
 // Assignment Code Here
 
 
-
-var numbers = ("Would you like to add numbers?");
-
-var lowerChars = ("Would you like to add lower case characters?");
-
-var upperChars = ("Would you like to add upper case characters?");
-
-var specialChars = ("Would you like to add special characters?");
-
-
 function generatePassword(){
   var lowerCase =  ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var upperCase =  ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*' , '(', ')', '"' ];
   var numbers =  ['1', '2', '3', '4', '5' , '6', '7' , '8', '9', '0' ];
+  var charactersForPassword = [];
   
   charsAmount = (prompt("To generate a password, how many characters would you like to add?", "NOTE: must between 8 - 128 characters"));
   if (charsAmount <= 8 || confirmLength >= 128 ) {
     alert("Password length must be between 8-128 characters Try again");
-    var charsAmount = (prompt("How many characters would you like your password to contain?"));
+    var charsAmount = (prompt("How many characters would you like your password to contain?")); 
+  }
+
+    else if (isNan (charsAmount)) {
+      charsAmount = prompt("please enter a vaild number")
+
+    }
+
+    numbers = (confirm("Would you like to add numbers"))
+    if (numbers) {
+      numbers = alert("Your password will have numbers")
+
+    }
+
+    lowerCase = (confirm ("Would you like to add lower case characters?"));
+    if (Lowercase) {
+       lowerCase = alert("Your password will have lowercase characters.");
+
     } 
 
+    upperCase = (confirm ("Would you like to add upper case characters?"));
+    if (upperCase) {
+      upperCase = alert("Your password will have upper case characters")
+
+    }
+
+    specialChars = (confirm("Would you like to add special characters?"))
+    if (specialChars){
+      specialChars = alert("Your password will have special characters")
+    }
+
+    if (numbers === false && lowerCase === false && upperCase === false && specialChars === false) {
+      return "Please select at least one character type.";
+
+    };
+
+
   
-}
+
+
+
 
 
 
